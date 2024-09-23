d4fdf41ac3efe78f9239ecdac2d37d10eab8b200
jkuya <kuyajesee@proton.me>
2024-09-23 14:46:12 +0300

    Feat: declare a flagset f

--- main.go
+++ main.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
+	"os"
 )
 
 func main() {
-	l := flag.String("l", "8080",  "Listen mode, for inbound connects")
-	flag.Parse()
-	if *l != "" {
-		fmt.Println(*l)
-	}
+	f := &flag.FlagSet{}
+
+	var l string
+	f.StringVar(&l, "l", "8080", "Listen mode, for inbound connects")
+	buf := bytes.NewBuffer([]byte{})
+
+	f.SetOutput(buf)
+	err := f.Parse(os.Args[1:])
+
+	if err == nil {
+		fmt.Println(l)
+	}else {
+                fmt.Println("nc: missing port number")
+                return
+        }
 }
