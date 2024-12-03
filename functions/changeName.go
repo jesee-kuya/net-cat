67ec857f1f2c640a1f87fab0853bfd59fa50764a
egathang <elyg3672@gmail.com>
2024-12-03 17:41:26 +0300

    Feat: add change name function.

---
+++ functions/changeName.go
@@ -0,0 +1,29 @@
+package netcat
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+)
+
+func ChangeName(client Client) ([]Client, Client, error) {
+	ext := fmt.Sprintf("%v has left the chat\n", client.name)
+	client.conn.Write([]byte("[ENTER NEW NAME]: "))
+	name, err := bufio.NewReader(client.conn).ReadString('\n')
+	if err != nil {
+		client.conn.Write([]byte("Connection Denied"))
+		client.conn.Close()
+		Chat(client.conn, ext)
+		Clients = Remove(Clients, client.conn)
+		return Clients, client, fmt.Errorf("Connection closed")
+	}
+	name = strings.TrimSpace(name)
+
+	for i := 0; i < len(Clients); i++ {
+		if Clients[i].conn == client.conn {
+			Clients[i].name = name
+			client.name = name
+		}
+	}
+	return Clients, client, nil
+}
