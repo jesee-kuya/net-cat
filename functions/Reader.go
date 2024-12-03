41f3ea99413366ed01ea55f0c9d90ea72675b9f2
egathang <elyg3672@gmail.com>
2024-12-03 17:38:39 +0300

    Feat: change name feature.

--- functions/Reader.go
+++ functions/Reader.go
@@ -8,6 +8,8 @@ import (
 	"time"
 )
 
+var History []string
+
 func Reader(conn net.Conn) {
 	var client Client
 	client.conn = conn
@@ -16,18 +18,24 @@ func Reader(conn net.Conn) {
 		conn.Write([]byte(fmt.Sprintf("%v", err)))
 		return
 	}
+
 	conn.Write([]byte(entry))
 	name, err := bufio.NewReader(conn).ReadString('\n')
 	if err != nil {
 		conn.Write([]byte("Connection Denied"))
 		return
 	}
-	name = strings.TrimSpace(name)
-	client.name = name
 	ent := fmt.Sprintf("%v has joined the chat\n", name)
 	ext := fmt.Sprintf("%v has left the chat\n", name)
+	name = strings.TrimSpace(name)
+	client.name = name
 	Chat(conn, ent)
 	Clients = append(Clients, client)
+
+	if len(History) != 0 {
+		history := strings.Join(History, "")
+		conn.Write([]byte(history))
+	}
 
 	for {
 		message, err := bufio.NewReader(conn).ReadString('\n')
@@ -37,7 +45,20 @@ func Reader(conn net.Conn) {
 			conn.Close()
 			break
 		}
-		t := fmt.Sprintf("[%v][%v]: %v", time.Now().Format("2006-01-02 15:04:05"), client.name, message)
-		Chat(client.conn, t)
+		if message != "" {
+			if strings.Contains(message, "--cn") {
+				old := client.name
+				Clients, client, err = ChangeName(client)
+				if err != nil {
+					break
+				}
+				cn := fmt.Sprintf("%v changed name to %v\n", old, client.name)
+				Chat(conn, cn)
+				continue
+			}
+			text := fmt.Sprintf("[%v][%v]: %v", time.Now().Format("2006-01-02 15:04:05"), client.name, message)
+			History = append(History, text)
+			Chat(client.conn, text)
+		}
 	}
 }
