eace8e0c0c3a38d4ad3c8d86ac3a0f37e048d00f
barraotieno <barrackotieno160@gmail.com>
2024-12-04 11:18:23 +0300

    Refactor: remove unnecessary in calling to avoid panics

--- functions/changeName.go
+++ functions/changeName.go
@@ -7,14 +7,9 @@ import (
 )
 
 func ChangeName(client Client) ([]Client, Client, error) {
-	ext := fmt.Sprintf("%v has left the chat\n", client.name)
 	client.conn.Write([]byte("[ENTER NEW NAME]: "))
 	name, err := bufio.NewReader(client.conn).ReadString('\n')
 	if err != nil {
-		client.conn.Write([]byte("Connection Denied"))
-		client.conn.Close()
-		Chat(client.conn, ext)
-		Clients = Remove(Clients, client.conn)
 		return Clients, client, fmt.Errorf("connection closed")
 	}
 	name = strings.TrimSpace(name)
