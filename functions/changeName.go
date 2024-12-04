3f8c8ab9f77635815a0ad460153166a7ac697e26
jkuya <kuyajesee@proton.me>
2024-12-04 14:36:32 +0300

    Feat: check the existence of the name

--- functions/changeName.go
+++ functions/changeName.go
@@ -7,12 +7,21 @@ import (
 )
 
 func ChangeName(client Client) ([]Client, Client, error) {
+	count := 0
 	client.conn.Write([]byte("[ENTER NEW NAME]: "))
 	name, err := bufio.NewReader(client.conn).ReadString('\n')
 	if err != nil {
 		return Clients, client, fmt.Errorf("connection closed")
 	}
 	name = strings.TrimSpace(name)
+
+	for !CheckName(name) {
+		if count == 5 {
+			return Clients, client, fmt.Errorf("connection closed")
+		}
+		client.conn.Write([]byte("Invalid name try another name\n[ENTER NEW NAME]: ]"))
+		count++
+	}
 
 	for i := 0; i < len(Clients); i++ {
 		if Clients[i].conn == client.conn {
