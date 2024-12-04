651417ddaa40806053498bc1de7f7b73e13fa77a
barraotieno <barrackotieno160@gmail.com>
2024-12-04 12:36:01 +0300

    Refactor: correctly format the code

--- functions/Reader.go
+++ functions/Reader.go
@@ -15,7 +15,8 @@ func Reader(conn net.Conn) {
 	client.conn = conn
 	entry, err := Readfile("./src/logo.txt")
 	if err != nil {
-		conn.Write([]byte(fmt.Sprintf("%v", err)))
+		conn.Write([]byte(fmt.Sprintf("Internal server error: %v", err)))
+		conn.Close()
 		return
 	}
 
@@ -23,12 +24,12 @@ func Reader(conn net.Conn) {
 	name, err := bufio.NewReader(conn).ReadString('\n')
 	if err != nil {
 		conn.Write([]byte("Connection Denied"))
+		conn.Close()
 		return
 	}
 
 	name = strings.TrimSpace(name)
 	ent := fmt.Sprintf("%v has joined the chat\n", name)
-	ext := fmt.Sprintf("%v has left the chat\n", name)
 	client.name = name
 	Chat(conn, ent)
 	Clients = append(Clients, client)
@@ -39,25 +40,32 @@ func Reader(conn net.Conn) {
 	}
 
 	for {
-		w := bufio.NewReader(conn)
-		message, err := w.ReadString('\n')
+		ext := fmt.Sprintf("%v has left the chat\n", client.name)
+		message, err := bufio.NewReader(conn).ReadString('\n')
 		if err != nil {
 			Chat(conn, ext)
 			Clients = Remove(Clients, conn)
 			conn.Close()
 			break
 		}
+
 		if message != "" {
+
 			if strings.Contains(message, "--cn") {
 				old := client.name
 				Clients, client, err = ChangeName(client)
 				if err != nil {
+					client.conn.Write([]byte("Connection Denied"))
+					client.conn.Close()
+					Chat(client.conn, ext)
+					Clients = Remove(Clients, client.conn)
 					break
 				}
 				cn := fmt.Sprintf("%v changed name to %v\n", old, client.name)
 				Chat(conn, cn)
 				continue
 			}
+			
 			text := fmt.Sprintf("[%v][%v]: %v", time.Now().Format("2006-01-02 15:04:05"), client.name, message)
 			History = append(History, text)
 			conn.Write([]byte("\033[F\033[K"))
